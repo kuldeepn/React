70b4bfe6bcb0397d67a0a60d965f40fc721f2796
kuldeepn <nandeshwar.kuldip@gmail.com>
2022-06-28 16:40:03 +0530

    a

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import Button from "../UI/Button/Button";
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
 
 const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };

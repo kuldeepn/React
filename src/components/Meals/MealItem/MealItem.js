d9ea2cd359e985bd2b67ef60f54906bcc099d5c1
kuldeepn <nandeshwar.kuldip@gmail.com>
2022-07-15 13:08:10 +0530

    15th july

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,15 @@
+const MealItem = (props) => {
+  const price = `₹${props.price.toFixed(2)}`;
 
-const MealItem = () => {
-        
-}
+  return (
+    <li>
+      <div>{props.name}</div>
+      <div>{props.description}</div>
+      <div>{price}</div>
+
+      <div></div>
+    </li>
+  );
+};
 
 export default MealItem;
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,5 @@
+
+
+MealItemForm = (params) => {
+     return 
+}

1c29ecf7992f2c7e51549e95e12ae092dbd4e388
kuldeepn <nandeshwar.kuldip@gmail.com>
2022-09-04 02:44:40 +0530

    .

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,39 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+
+  const enteredValid = useRef();
+
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      return;
+    }
+
+    const usingRef =enteredValid.current.value;
+    console.log(enteredName);
+    console.log(usingRef);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={enteredValid}
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
